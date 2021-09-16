b56daee6b24e1451d85294f582836e0ebc9eac13
Emiliooo90 <emi.romero@duocuc.cl>
2021-09-15 23:45:10 -0300

    Se hacen las validaciones

--- src/app/ingreso/ingreso.page.ts
+++ src/app/ingreso/ingreso.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ingreso',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingreso.page.scss'],
 })
 export class IngresoPage implements OnInit {
+  validacion = String;
 
-  constructor() { }
+  constructor(
+    private alertControl: AlertController
+  ) { }
 
   ngOnInit() {
+  }
+  async validar (nombre: HTMLInputElement, clave: HTMLInputElement)
+  {
+    let name = nombre.value;
+    let password = clave.value;
+
+     if (name.trim() == "")
+     {
+       const alerta = await this.alertControl.create({
+         header: "Falta información",
+         message: "Por favor ingrese información en el campo nombre",
+       });
+       await alerta.present();
+     }
+
+     else if (password.trim() == "")
+     {
+       const alerta = await this.alertControl.create({
+         header: "Falta información",
+         message: "Por favor ingrese información en el campo contraseña"
+       });
+       await alerta.present();
+     }
   }
 
 }
